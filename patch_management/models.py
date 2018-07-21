7824240968127c4c38eb997082605bf4d41647bc
Soranut Midtrapanon <porpreing@gmail.com>
2018-07-21 20:01:05 +0100

    Deleted my test model

--- patch_management/models.py
+++ patch_management/models.py
@@ -23,17 +23,3 @@ def save_user_ssh_profile(sender, instance, **kwargs):
 class Server(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, related_name='servers', on_delete=models.CASCADE)
-
-
-
-
-
-class MyModel(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    description = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True)
-    message = models.TextField(null=True)
-
-    def __str__(self):
-        return self.name
