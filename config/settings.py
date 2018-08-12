85a443ce4258d86cd96644b43a729aff9e7ce585
Soranut Midtrapanon <porpreing@gmail.com>
2018-08-12 02:10:21 +0100

    Turned off DEBUG

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1d2rbq-$&ya5n=idl6qsap19du8t@en*stgl)bbxg-#ex$(dyl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
